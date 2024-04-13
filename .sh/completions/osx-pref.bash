d4721ba1822f371e848a5d2c5ee604bdb6a47a4b
Shawn Conn <shawn@luciditi.io>
2024-04-13 17:20:07 -0400

    config

--- .sh/completions/osx-pref.bash
+++ .sh/completions/osx-pref.bash
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 
-# Complete osx-pref based on pref pane dir.
-_osx-pref_completions()
+# Complete macos-pref based on pref pane dir.
+_macos-pref_completions()
 {
   if [[ -d "/System/Library/PreferencePanes" ]]; then
     PREF_PANES=$(ls /System/Library/PreferencePanes/ | sed -e 's/\.prefPane//g')
@@ -10,4 +10,4 @@ _osx-pref_completions()
   fi
   COMPREPLY=($(compgen -W "$PREF_PANES" "${COMP_WORDS[1]}"))
 }
-complete -F _osx-pref_completions osx-pref
+complete -F _macos-pref_completions macos-pref
